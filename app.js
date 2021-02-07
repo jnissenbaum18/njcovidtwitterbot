6b79aaac530845dba856af27c82cae92fa0ab9b2
jnissenbaum18 <jnissenbaum@verizon.net>
2021-02-07 02:52:26 -0500

    added account data get and set

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { streamInit } = require("./src/twitter");
 const dotenv = require("dotenv").config();
-const { registerUser, login } = require("./src/auth");
+const { registerUser, login, validateToken } = require("./src/auth");
 const { formatPhoneNumber } = require("./src/utils");
 
 // Include the cluster module
@@ -69,40 +69,16 @@ if (
     });
   });
 
-  app.post("/account", async function (req, res) {
-    const email = req.body.email;
-    console.log("get acct data", ddbTable, req.body.email);
-    if (email) {
-      ddb.getItem(
-        {
-          TableName: ddbTable,
-          Key: {
-            email: { S: email },
-          },
-        },
-        function (err, data) {
-          if (err) {
-            var returnStatus = 500;
-
-            res.status(returnStatus).end();
-            console.log("DDB Error: " + err);
-          } else {
-            res.send(data);
-          }
-        }
-      );
-    }
-  });
-
   app.post("/login", async function (req, res) {
     console.log(req.body);
     const email = req.body.email;
     const password = req.body.password;
     try {
       const cognitoTokens = await login(email, password);
-      //Do not send idToken to client
+      //Not sure if ID token should be stored client side
       console.log("cognitoTokens ", cognitoTokens);
       res.send({
+        idToken: cognitoTokens.idToken,
         accessToken: cognitoTokens.accessToken,
         refreshToken: cognitoTokens.refreshToken,
       });
@@ -165,6 +141,86 @@ if (
                 }
               }
             );
+          }
+        }
+      );
+    }
+  });
+
+  app.post("/account", async function (req, res) {
+    const idToken = req.body.idToken;
+    console.log("get acct data", ddbTable, req.body);
+    const validToken = await validateToken(idToken);
+    console.log("valid token ", validToken);
+
+    if (validToken) {
+      ddb.getItem(
+        {
+          TableName: ddbTable,
+          Key: {
+            email: { S: validToken.email },
+          },
+        },
+        function (err, data) {
+          if (err) {
+            var returnStatus = 500;
+
+            res.status(returnStatus).end();
+            console.log("DDB Error: " + err);
+          } else {
+            if (data) {
+              console.log("account data ", data);
+              res.send({
+                email: data.Item.email.S,
+                phone: data.Item.phone.S,
+              });
+            }
+          }
+        }
+      );
+    }
+  });
+
+  app.post("/account-update", async function (req, res) {
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const idToken = req.body.idToken;
+    console.log("get acct data", ddbTable, req.body);
+    const validToken = await validateToken(idToken);
+    console.log("valid token ", validToken);
+
+    if (validToken) {
+      ddb.updateItem(
+        {
+          TableName: ddbTable,
+          UpdateExpression: "set phone = :p",
+          ExpressionAttributeValues: {
+            ":p": {
+              S: phone,
+            },
+          },
+          ReturnValues: "UPDATED_NEW",
+          Key: {
+            email: { S: validToken.email },
+          },
+        },
+        function (err, data) {
+          if (err) {
+            var returnStatus = 500;
+
+            res.status(returnStatus).end();
+            console.log("DDB Error: " + err);
+          } else {
+            if (data) {
+              console.log(data);
+              res.send({
+                success: true,
+              });
+              //   res.send({
+              //     email: data.email.S,
+              //     phone: data.phone.S,
+              //   });
+            }
           }
         }
       );
